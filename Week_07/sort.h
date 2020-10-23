b1e91d330de1fb93febe820cb789fdcacba15edb
Diego F. Aranha <dfaranha@gmail.com>
2020-10-23 04:54:33 +0200

    Added array sorting.

---
+++ Week_07/sort.h
@@ -0,0 +1,17 @@
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * Several sorting routines.
+ * Arrays are rearranged with smallest item first.
+ */
+
+#include <vector>
+
+using namespace std;
+
+#include "insertion_sort.h"
+#include "merge_sort.h"
+#include "quick_sort.h"
+
+#endif
